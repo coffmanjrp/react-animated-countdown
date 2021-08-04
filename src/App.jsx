edb3b0cb8f6ba6d803a7a3d4dcfe3a2f21c21a87
coffmanjrp <coffmanjrp@gmail.com>
2021-08-03 23:54:34 -0700

    Implment HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.scss';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <>
+      <div className="counter">
+        <div className="nums">
+          <span className="in">3</span>
+          <span>2</span>
+          <span>1</span>
+          <span>0</span>
+        </div>
+        <h4>Get Ready</h4>
+      </div>
+      <div className="final">
+        <h1>GO</h1>
+        <button className="btn">Replay</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
